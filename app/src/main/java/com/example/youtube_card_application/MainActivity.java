f3c79f7cedc16e9e6605606cd1c7022ee88cef12
sarvesh8791 <sarveshmaurya8791@gmail.com>
2019-03-12 00:25:15 +0530

    code3

--- app/src/main/java/com/example/youtube_card_application/MainActivity.java
+++ app/src/main/java/com/example/youtube_card_application/MainActivity.java
@@ -8,16 +8,34 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     CardView card1;
+    CardView card2;
+    CardView card3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         card1=findViewById(R.id.card1);
+        card2=findViewById(R.id.card2);
+        card3=findViewById(R.id.card3);
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             Intent intent=new Intent(MainActivity.this,information.class);
             startActivity(intent);
+            }
+        });
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,informaation2.class);
+                startActivity(intent);
+            }
+        });
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,information3.class);
+                startActivity(intent);
             }
         });
     }
